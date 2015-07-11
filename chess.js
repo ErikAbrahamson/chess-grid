750fc43b15b38c03adac3cfc054c95a6ba45501a
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-07-10 18:37:40 -0600

    Ugh

---
+++ chess.js
@@ -0,0 +1,24 @@
+function chessGrid(width,height) {
+var even = ' ', odd = '#', down = '\n';
+  for (var h=0; h<=height; h++) {
+    var line = '';
+
+    for (var w=0; w<=width; w++) {
+      if (w % 2 === 0) {
+        line +=even;
+      } else {
+        line +=odd;
+      }
+    }
+    line +=down;
+    console.log(line);
+    if (even === '#') {
+      var even = ' ', odd = '#';
+    } else {
+      var even = '#', odd = ' ';
+    }
+  }
+}
+
+//test
+chessGrid(8,8);
