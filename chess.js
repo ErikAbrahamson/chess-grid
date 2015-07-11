3aadbbc82c494840bbc67c2004118c8d3ace8904
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-07-11 17:12:35 -0600

    Solution for chess grid

--- chess.js
+++ chess.js
@@ -1,17 +1,18 @@
+// My solution for chess-board
 function chessGrid(width, height) {
-var even = ' ', odd = '#', down = '\n';
+  var even = ' ', odd = '#', down = '\n';
   for (var h = 0; h <= height; h++) {
     // 'line' must be defined inside this loop to initiate
-    var line = '';
+    var row = '';
     for (var w=0; w<=width; w++) {
       if (w % 2 === 0) {
-        line +=even;
+        row +=even;
       } else {
-        line +=odd;
+        row +=odd;
       }
     }
-    line +=down;
-    console.log(line);
+    row +=down;
+    console.log(row);
     if (even === '#') {
       var even = ' ', odd = '#';
     } else {
